265517f050730abfe2fc3d9c1ef2690d75e54831
Vladimir <35892932+Overcllock@users.noreply.github.com>
2018-07-09 09:55:34 +0300

    Anim test

---
+++ Assets/src/AnimTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour {
+	private Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Jab"))
+				anim.Play("Hikick", 0);
+			else if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+				anim.Play("Jab", 0);
+		}
+		else if(Input.GetMouseButtonDown(1))
+			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+				anim.Play("Spinkick", 0);
+	}
+}
