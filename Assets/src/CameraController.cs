2b577fbb9ecadc61e9c7683eeb1edff35bb9b841
Overcllock <vz.wct@yandex.ru>
2018-07-10 16:28:56 +0300

    Adding basic camera controller

---
+++ Assets/src/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace game
+{
+	public class CameraController : MonoBehaviour 
+	{
+		Camera cam;
+		MouseLook m_look;
+		CharacterController cctl;
+
+		void Start () 
+		{
+			cam = gameObject.GetChild("camera").GetComponent<Camera>();
+			cctl = gameObject.GetComponent<CharacterController>();
+			m_look = new MouseLook();
+			m_look.Init(cctl.transform, cam.transform);
+		}
+		
+		void FixedUpdate () 
+		{
+			m_look.LookRotation(cctl.transform, cam.transform);
+		}
+	}
+}
---
+++ Assets/src/MouseLook.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+namespace game
+{
+    public class MouseLook
+    {
+        float XSensitivity = 1.1f;
+        float YSensitivity = 1.1f;
+        float MinimumX = -45F;
+        float MaximumX = 45F;
+        float smoothTime = 11f;
+
+        Quaternion m_CharacterTargetRot;
+        Quaternion m_CameraTargetRot;
+        bool m_cursorIsLocked = true;
+
+        public void Init(Transform character, Transform camera)
+        {
+            m_CharacterTargetRot = character.localRotation;
+            m_CameraTargetRot = camera.localRotation;
+        }
+
+
+        public void LookRotation(Transform character, Transform camera)
+        {
+            float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+            float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+
+            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+
+            UpdateRotation(ref character, m_CharacterTargetRot);
+            UpdateRotation(ref camera, m_CameraTargetRot);
+
+            UpdateCursorLock();
+        }
+
+        void UpdateRotation(ref Transform source, Quaternion target)
+        {
+            source.localRotation = Quaternion.Slerp(
+                source.localRotation, 
+                target,
+                smoothTime * Time.deltaTime
+            );
+        }
+
+        void UpdateCursorLock()
+        {
+            m_cursorIsLocked = !Input.GetKey(KeyCode.LeftAlt);
+
+            Cursor.lockState = m_cursorIsLocked ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !m_cursorIsLocked;
+        }
+
+        void FixY()
+        {
+            
+        }
+
+        Quaternion ClampRotationAroundXAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+            angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+            q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+            return q;
+        }
+    }
+}
