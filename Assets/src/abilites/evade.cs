1f68b6eaa5d37c0b35d9c095a63d4f794e3f1b53
Overcllock <vz.wct@yandex.ru>
2018-08-20 15:56:42 +0300

    Extract evade abilites

---
+++ Assets/src/abilites/evade.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+namespace game
+{
+    public abstract class Evade : Ability
+	{
+		const float EVADING_SPEED = 3.1f;
+		const float EVADING_ANGLE = 180.0f;
+
+        protected override void Use()
+		{
+			base.Use();
+			inflictor.is_invulnerable = true;
+			inflictor.mctl.moving_allowed = false;
+			inflictor.mctl.keep_camera_look_at = true;
+		}
+
+		public override void Tick(float dt)
+		{
+			base.Tick(dt);
+			if(inflictor.hud != null)
+				inflictor.hud.UpdateSkillButtonAlpha(conf.axis, CheckConditions());
+		}
+
+		public override bool CheckConditions()
+		{
+			return inflictor.HasTargetInRadius(conf.radius);
+		}
+
+		public override void Defer()
+		{
+			inflictor.is_invulnerable = false;
+			inflictor.mctl.moving_allowed = true;
+			inflictor.mctl.keep_camera_look_at = false;
+		}
+
+		protected IEnumerator Move(Vector3 point, Vector3 axis, float delay)
+		{
+			float ttl = 0;
+			do
+			{
+				inflictor.transform.RotateAround(point, axis, Time.fixedDeltaTime * EVADING_SPEED * EVADING_ANGLE);
+				ttl += Time.fixedDeltaTime;
+				yield return new WaitForFixedUpdate();
+			}
+			while(ttl < delay);
+		}
+	}
+}
--- Assets/src/abilites/rightevade/common.cs
+++ Assets/src/abilites/rightevade/common.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using System.Collections;
 
 namespace game
 {
-    public class RightEvade : Ability
+    public class RightEvade : Evade
 	{
 		const string CONF_PATH = "Assets/src/abilites/rightevade/conf.json";
 
@@ -15,31 +16,9 @@ namespace game
 		protected override void Use()
 		{
 			base.Use();
-			inflictor.is_invulnerable = true;
-			inflictor.mctl.moving_allowed = false;
-			inflictor.mctl.keep_camera_look_at = true;
 
 			var target = inflictor.FindNearestTarget(conf.radius);
-			inflictor.StartCoroutine(inflictor.Evade(target.transform.position, -Vector3.up, conf.delay / 2));
-		}
-
-		public override void Tick(float dt)
-		{
-			base.Tick(dt);
-			if(inflictor.hud != null)
-				inflictor.hud.UpdateSkillButtonAlpha(conf.axis, CheckConditions());
-		}
-
-		public override bool CheckConditions()
-		{
-			return inflictor.HasTargetInRadius(conf.radius);
-		}
-
-		public override void Defer()
-		{
-			inflictor.is_invulnerable = false;
-			inflictor.mctl.moving_allowed = true;
-			inflictor.mctl.keep_camera_look_at = false;
+			inflictor.StartCoroutine(Move(target.transform.position, -Vector3.up, conf.delay / 2));
 		}
 	}
 }
